f698d6116aa041aad71dbb8c3edfe7913e24a87e
jh1173 <74472579+jh1173@users.noreply.github.com>
2020-12-01 23:46:02 -0500

    Update Editor class
    Added lots of (untested) functionality to Editor class, added sound classes

--- src/Editor.java
+++ src/Editor.java
@@ -23,35 +23,14 @@ public class Editor {
 	private JLabel wordLabel, soundLabel, recordLengthLabel, sentenceLabel, levelLabel;
 	private JTextField wordField, levelField, recordLengthField;
 	private JButton previewSound, recordSound, previewSentence, recordSentence, saveButton, cancelButton;
+	private String currentSpelling;
+	private Integer currentLevel;
 
 	public Editor() {
-		// get all sound
-		//HashMap<String, Clip> pronunciations = getClips("Recordings");
-		//HashMap<String, Clip> sentenceMap = getClips("Sentences");
 		// load words and create levels
-		File wordFolder = new File("Words");
-		File[] levelFiles = wordFolder.listFiles();
-		for (File levelFile: levelFiles) {
-			String fileName = levelFile.getName();
-			Integer levelNum = Integer.valueOf(fileName.substring(0, fileName.indexOf(".")));
-			levelList.addElement(levelNum);
-			DefaultListModel<Word> levelWordList = new DefaultListModel<>();
-			try (Scanner levelScanner = new Scanner(levelFile)){
-				while (levelScanner.hasNextLine()) {
-					 String spelling = levelScanner.nextLine();
-					 //Clip sound = pronunciations.get(spelling);
-					 //Clip sentence = sentenceMap.get(spelling);
-					 levelWordList.addElement(new Word(spelling));
-					 //levelWordList.addElement(new Word(spelling, sound, sentence));
-				}
-			}
-			catch (FileNotFoundException ex1) {
-				
-			}
-			wordList.put(levelNum, levelWordList);
-		}
+		loadWords();
 		// ==========set up frame==========
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // FUTURE warn before closing, cancel edit word upon close
 		mainMenu.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		mainMenu.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
@@ -65,15 +44,17 @@ public class Editor {
 		levels = new JList<>(levelList);
 		levels.setLayoutOrientation(JList.HORIZONTAL_WRAP);
 		levels.setVisibleRowCount(-1);
-		levels.setFixedCellWidth(25);
+		levels.setFixedCellWidth(30);
 		DefaultListCellRenderer renderer = (DefaultListCellRenderer) levels.getCellRenderer();
 		renderer.setHorizontalAlignment(SwingConstants.CENTER);
 		levels.addListSelectionListener(new ListSelectionListener() {
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
 				if (!e.getValueIsAdjusting()) {
-					Integer key = levels.getSelectedValue();
-					words.setModel(wordList.get(key));
+					if (!levelList.isEmpty()) {
+						Integer key = levels.getSelectedValue();
+						words.setModel(wordList.get(key));
+					}					
 				}
 			}
 		});
@@ -133,9 +114,13 @@ public class Editor {
 		// save and cancel buttons
 		saveButton = new JButton("Save");
 		editMenu.add(saveButton, c);
+		saveButton.addActionListener(new CloseEditMenu());
+		saveButton.setActionCommand("save");
 		c.gridx++;
 		cancelButton = new JButton("Cancel");
 		editMenu.add(cancelButton, c);
+		cancelButton.addActionListener(new CloseEditMenu());
+		cancelButton.setActionCommand("cancel");
 		// text fields
 		c.gridx = 1; c.gridy = 0;
 		c.gridwidth = 2;
@@ -152,22 +137,35 @@ public class Editor {
 		c.gridy = 1;
 		previewSound = new JButton("Preview");
 		editMenu.add(previewSound, c);
+		previewSound.addActionListener(new PreviewSound());
+		previewSound.setActionCommand("spelling");
 		c.gridx++;
 		recordSound = new JButton("Record");
 		editMenu.add(recordSound, c);
+		recordSound.addActionListener(new RecordSound());
+		recordSound.setActionCommand("spelling");
 		c.gridx = 1; c.gridy = 2;
 		previewSentence = new JButton("Preview");
 		editMenu.add(previewSentence, c);
+		previewSentence.addActionListener(new PreviewSound());
+		previewSentence.setActionCommand("sentence");
 		c.gridx++;
 		recordSentence = new JButton("Record");
 		editMenu.add(recordSentence, c);
+		recordSentence.addActionListener(new RecordSound());
+		recordSentence.setActionCommand("sentence");
 		// set main menu
 		frame.setContentPane(mainMenu);
 		frame.pack();
 		frame.setVisible(true);
 	}
 	
-	public static HashMap<String, Clip> getClips(String audioFolder){
+	/*
+	 * get set of key-value pairs of name of sound file and associated clip
+	 * @param audioFolder
+	 * @return
+	 */
+	/*public static HashMap<String, Clip> getClips(String audioFolder){
 		HashMap<String, Clip> clipMap = new HashMap<>();
 		File soundDirectory = new File(audioFolder);
 		File[] soundFiles = soundDirectory.listFiles();
@@ -194,19 +192,154 @@ public class Editor {
 		}
 		return clipMap;
 	}
+	*/
 	
+	/**
+	 * put words from word files into class attributes (lists)
+	 */
+	private void loadWords() {
+		levelList.clear();
+		wordList.clear();
+		File wordFolder = new File("Words");
+		File[] levelFiles = wordFolder.listFiles();
+		for (File levelFile: levelFiles) {
+			// load level
+			String fileName = levelFile.getName();
+			Integer levelNum = Integer.valueOf(fileName.substring(0, fileName.indexOf(".")));
+			levelList.addElement(levelNum);
+			// load words in level
+			DefaultListModel<Word> levelWordList = new DefaultListModel<>();
+			try (Scanner levelScanner = new Scanner(levelFile)) {
+				while (levelScanner.hasNextLine()) {
+					 String spelling = levelScanner.nextLine();
+					 levelWordList.addElement(new Word(spelling));
+				}
+			}
+			catch (FileNotFoundException ex1) {
+				
+			}
+			wordList.put(levelNum, levelWordList);
+		}
+	}
+	
+	/**
+	 * remove word from level file
+	 * @param word
+	 * @param level
+	 */
+	private void removeWord(String word, Integer level) {
+		// set up files
+		File wordLevel = new File("Words/" + level.toString() + ".txt");
+		File levelCopy = new File("Words/copy.txt");
+		try {
+			levelCopy.createNewFile();
+		}
+		catch (IOException ex) {
+			
+		}
+		// copy level file without word
+		try (Scanner levelScanner = new Scanner(wordLevel);
+				PrintWriter copyPrinter = new PrintWriter(levelCopy);){
+			while (levelScanner.hasNextLine()) {
+				String nextWord = levelScanner.nextLine();
+				if (!nextWord.equals(word)) {
+					copyPrinter.println(nextWord);
+				}
+			}
+		}
+		catch (FileNotFoundException ex) {
+
+		}
+		// copy back to level
+		boolean lastRemoved = true;
+		try (Scanner copyScanner = new Scanner(levelCopy);
+				PrintWriter levelPrinter = new PrintWriter(wordLevel);){
+			while (copyScanner.hasNextLine()) {
+				lastRemoved = false;
+				levelPrinter.println(copyScanner.nextLine());
+			}
+		}
+		catch (FileNotFoundException ex) {
+
+		}
+		System.gc();
+		levelCopy.delete();
+		if (lastRemoved) {
+			wordLevel.delete();
+		}
+	}
+	
+	/**
+	 * add word to level file
+	 * @param word
+	 * @param level
+	 */
+	private void addWord(String word, Integer level) {
+		File wordLevel = new File("Words/" + level.toString() + ".txt");
+		File levelCopy = new File("Words/copy.txt");
+		try {
+			levelCopy.createNewFile();
+		}
+		catch (IOException ex) {
+			
+		}
+		// copy level with extra word
+		try (Scanner levelScanner = new Scanner(wordLevel);
+				PrintWriter copyPrinter = new PrintWriter(levelCopy);){
+			while (levelScanner.hasNextLine()) {
+				String nextWord = levelScanner.nextLine();
+				copyPrinter.println(nextWord);
+			}
+			copyPrinter.println(word);
+		}
+		catch (FileNotFoundException ex) {
+
+		}
+		// copy back to level
+		try (Scanner copyScanner = new Scanner(levelCopy);
+				PrintWriter levelPrinter = new PrintWriter(wordLevel);){
+			while (copyScanner.hasNextLine()) {
+				levelPrinter.println(copyScanner.nextLine());
+			}
+		}
+		catch (FileNotFoundException ex) {
+
+		}
+		System.gc();
+		levelCopy.delete();
+	}
+	
+	// TODO implement remove word, confirm remove
+	
+	/**
+	 * Attached to add and edit buttons
+	 */
 	private class OpenEditMenu implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// load current word if necessary
 			// audio clips can be loaded by file
+			File newSound = new File("temp_word.wav");
+			File newSentence = new File("temp_sentence.wav");
+			System.gc();
+			newSound.delete();
+			newSentence.delete();
+			// load word info if available
 			Word currentWord = null;
-			Integer currentLevel = -1;
+			currentLevel = -1;
 			if (e.getActionCommand().equals("edit")) {
 				currentWord = words.getSelectedValue();
 				currentLevel = levels.getSelectedValue();
-				wordField.setText(currentWord.getWord());
+				currentSpelling = currentWord.getWord();
+				wordField.setText(currentSpelling);
 				levelField.setText(currentLevel.toString());
+			}
+			else {
+				wordField.setText("");
+				levelField.setText("");
+				previewSound.setEnabled(false);
+				previewSentence.setEnabled(false);
+				currentSpelling = null;
 			}
 			recordLengthField.setText("3");
 			frame.setContentPane(editMenu);
@@ -215,6 +348,114 @@ public class Editor {
 				frame.setTitle("Add Word");
 			} else {
 				frame.setTitle("Edit " + currentWord.getWord());
+			}
+		}
+	}
+	
+	/**
+	 * Attached to save and cancel buttons
+	 */
+	private class CloseEditMenu implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO make sure all information is present before saving, including no duplicate words
+			// FUTURE confirm cancel
+			// delete temp files
+			System.gc();
+			File newSound = new File("Recordings/" + wordField.getText() + ".wav");
+			File updatedSound = new File("temp_word.wav");
+			File newSentence = new File("Sentences/" + wordField.getText() + ".wav");
+			File updatedSentence = new File("temp_sentence.wav");
+			if (e.getActionCommand().equals("cancel")) {
+				updatedSound.delete();
+				updatedSentence.delete();
+			}
+			else {
+				newSound.delete();
+				updatedSound.renameTo(newSound);
+				newSentence.delete();
+				updatedSentence.renameTo(newSentence);
+			}
+			if (e.getActionCommand().equals("save")) {
+				// update word list
+				String newWord = wordField.getText();
+				Integer newLevel = Integer.parseInt(levelField.getText());
+				if (!currentSpelling.equals(wordField.getText())
+						|| !currentLevel.equals(newLevel)) {
+					removeWord(currentSpelling, currentLevel);
+					addWord(newWord, newLevel);
+				}
+				loadWords();
+				levels.setModel(levelList);
+				words.setModel(new DefaultListModel<>());
+			}
+			frame.setContentPane(mainMenu);
+			frame.pack();
+			frame.setTitle("Editor");
+			
+		}
+	}
+	
+	/**
+	 * Attached to preview buttons
+	 */
+	private class PreviewSound implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			SoundPlayback player = new SoundPlayback();
+			// find and play sound file
+			if (e.getActionCommand().equals("spelling")) {
+				File soundFile = new File("temp_word.wav");
+				if (currentSpelling == null || soundFile.exists()) {
+					player.play("temp_word.wav");
+				}
+				else {
+					player.play("Recordings/" + currentSpelling + ".wav");
+				}
+			}
+			else {
+				File sentenceFile = new File("temp_sentence.wav");
+				if (currentSpelling == null || sentenceFile.exists()) {
+					player.play("temp_sentence.wav");
+				}
+				else {
+					player.play("Recordings/" + currentSpelling + ".wav");
+				}
+				
+			}
+		}
+	}
+	
+	/**
+	 * Attached to record buttons
+	 */
+	private class RecordSound implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			SoundCapture recorder;
+			// create temporary sound file
+			if (e.getActionCommand().equals("spelling")) {
+				recorder = new SoundCapture("temp_word.wav");
+			}
+			else {
+				recorder = new SoundCapture("temp_sentence.wav");
+			}
+			// try to capture sound
+			try {
+				long recordTime = (long)(Double.parseDouble(recordLengthField.getText()) * 1000);
+				if (recordTime > 30000 || recordTime <= 0) {
+					throw new Exception();
+				}
+				recorder.startCapture(recordTime);
+				if (e.getActionCommand().equals("spelling")) {
+					previewSound.setEnabled(true);
+				}
+				else {
+					previewSentence.setEnabled(true);
+				}
+			}
+			catch (Exception ex) {
+				
 			}
 		}
 	}
@@ -229,3 +470,6 @@ public class Editor {
 	}
 
 }
+
+// FIXME lots and lots and lots of testing, make sure to create backup copies of files beforehand (it may already be too late)
+// Nothing has been tested yet because I'm scared about what may happen to the files
---
+++ src/SoundCapture.java
@@ -0,0 +1,104 @@
+import javax.sound.sampled.*;
+import java.io.*;
+
+/**
+ * A sample program is to demonstrate how to record sound in Java
+ * author: www.codejava.net
+ * https://www.codejava.net/coding/capture-and-record-sound-into-wav-file-with-java-sound-api
+ */
+public class SoundCapture {
+	
+    // path of the wav file
+    private File wavFile;
+ 
+    // format of audio file
+    private AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+ 
+    // the line from which audio data is captured
+    private TargetDataLine line;
+    
+    public SoundCapture(String filePath) {
+    	setFilePath(filePath);
+    }
+    
+    public void setFilePath(String filePath) {
+    	wavFile = new File(filePath);
+    }
+    /**
+     * Defines an audio format
+     */
+    private AudioFormat getAudioFormat() {
+        float sampleRate = 16000;
+        int sampleSizeInBits = 8;
+        int channels = 2;
+        boolean signed = true;
+        boolean bigEndian = true;
+        AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits,
+                                             channels, signed, bigEndian);
+        return format;
+    }
+    
+    /**
+     * Captures the sound and record into a WAV file
+     */
+    private void start() {
+        try {
+            AudioFormat format = getAudioFormat();
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+ 
+            // checks if system supports the data line
+            if (!AudioSystem.isLineSupported(info)) {
+                //System.out.println("Line not supported");
+                //System.exit(0);
+            }
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+            line.start();   // start capturing
+ 
+            //System.out.println("Start capturing...");
+ 
+            AudioInputStream ais = new AudioInputStream(line);
+ 
+            //System.out.println("Start recording...");
+ 
+            // start recording
+            AudioSystem.write(ais, fileType, wavFile);
+ 
+        } catch (LineUnavailableException ex) {
+            //ex.printStackTrace();
+        } catch (IOException ioe) {
+            //ioe.printStackTrace();
+        }
+    }
+ 
+    /**
+     * Closes the target data line to finish capturing and recording
+     */
+    private void finish() {
+        line.stop();
+        line.close();
+        //System.out.println("Finished");
+    }
+ 
+    /**
+     * Entry to run the program
+     */
+    public void startCapture(long recordTime) {
+        // creates a new thread that waits for a specified
+        // of time before stopping
+        Thread stopper = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(recordTime);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                finish();
+            }
+        });
+        stopper.start();
+        // start recording
+        start();
+    }
+
+}
